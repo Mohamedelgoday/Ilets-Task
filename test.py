98596bd948107666b5421f0873de5c4d00a32376
Ahmed-Adel-2040 <ahmed.adel.abosood@gmail.com>
2020-07-23 13:48:20 +0200

    action code is added

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import sys
+
+from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import  QApplication,QFrame
 from PyQt5.uic import loadUi
 
@@ -7,6 +9,14 @@ class Ilets(QFrame) :
         super(Ilets,self).__init__()
         loadUi("main.ui",self)
         self.setWindowTitle("ILETS Correct")
+        #intro_correct_button
+        self.intro_correct_button.clicked.connect(self.actionButton)
+    @pyqtSlot()
+    def actionButton(self):
+        self.intr_lexis_lable.setText("Ok Dev")
+        self.intro_grammer_lable.setText("Ok Gogo"+self.intro_textEdit.toPlainText ())
+        t=self.intro_textEdit.toPlainText ()
+        self.intro_textEdit.setText("Ok Gogo"+t)
 app=QApplication(sys.argv)
 widget=Ilets()
 widget.show()
