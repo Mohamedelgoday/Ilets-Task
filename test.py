96cfcf6b28f889aac5c361ff0678057b6428c9f8
Ahmed-Adel-2040 <ahmed.adel.abosood@gmail.com>
2020-07-24 14:50:25 +0200

    change code

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import  QApplication,QFrame
 from PyQt5.uic import loadUi
 
+@pyqtSlot()
 class Ilets(QFrame) :
     def __init__(self):
         super(Ilets,self).__init__()
@@ -11,12 +12,14 @@ class Ilets(QFrame) :
         self.setWindowTitle("ILETS Correct")
         #intro_correct_button
         self.intro_correct_button.clicked.connect(self.actionButton)
-    @pyqtSlot()
+
     def actionButton(self):
         self.intr_lexis_lable.setText("Ok Dev")
         self.intro_grammer_lable.setText("Ok Gogo"+self.intro_textEdit.toPlainText ())
         t=self.intro_textEdit.toPlainText ()
         self.intro_textEdit.setText("Ok Gogo"+t)
+
+    
 app=QApplication(sys.argv)
 widget=Ilets()
 widget.show()
