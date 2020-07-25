59c5439255bd2ea69bd0bf3efda3927288d5ff0d
Ahmed-Adel-2040 <ahmed.adel.abosood@gmail.com>
2020-07-25 14:04:46 +0200

    file added

---
+++ correct.py
@@ -0,0 +1,5 @@
+import language_check
+lang_tool = language_check.LanguageTool("en-US")
+text = "A sentence with a error in the Hitchhiker’s Guide tot he Galaxy"
+matches = lang_tool.check(text)
+print(matches)
